14981ed024e0662ea07cfe781ed62df761ef930c
jsadlocha <jrsadlocha@gmail.com>
2022-11-03 14:52:52 +0100

    Update in serial_monitor

--- tools/serial_monitor.py
+++ tools/serial_monitor.py
@@ -15,13 +15,3 @@ with serial.Serial('/dev/ttyACM0', 9600, timeout=1) as con:
 
 a = serial.Serial()
 con.close()
-
-# stty -F /dev/ttyACM0 cs8 115200 ignbrk -brkint -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
-
-# exec 5>/dev/ttyACM0 // open serial monitor
-# exec 5>&-  // close file
-# echo -ne "dupa\n" > /dev/ttyACM0 // write data
-# cat /dev/ttyACM0 // read data
-
-# sudo modprobe usbmon
-# sudo wireshark
